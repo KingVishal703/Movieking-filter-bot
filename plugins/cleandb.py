6804740e60f287e845f69508cdd08135a3eddb16
KingVishal703 <vishal21wankhede@gmail.com>
2025-07-04 01:35:19 +0530

    Update cleandb.py

--- plugins/cleandb.py
+++ plugins/cleandb.py
@@ -1,7 +1,14 @@
 from pyrogram import Client, filters
-from pyrogram.types import Message
-from config import ADMINS  # ya jahan se aapka admin ID define hai
+from pyrogram.types import *
+from info import ADMINS  # ya jahan se aapka admin ID define hai
 from plugins.database import db  # yeh wahi db hai jo aap use kar rahe ho
+
+
+from database.ia_filterdb import col, sec_col, get_file_details, unpack_new_file_id, get_bad_files
+from database.users_chats_db import db, delete_all_referal_users, get_referal_users_count, get_referal_all_users, referal_add_user
+from database.join_reqs import JoinReqs
+
+
 
 @Client.on_message(filters.command("cleandb") & filters.user(ADMINS))
 async def clean_database(client, message: Message):
