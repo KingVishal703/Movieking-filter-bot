3a772a83a3b99059c337a62559e25b916f894fd2
KingVishal703 <vishal21wankhede@gmail.com>
2025-07-04 01:25:28 +0530

    Update cleandb.py

--- plugins/cleandb.py
+++ plugins/cleandb.py
@@ -3,7 +3,7 @@ from pyrogram.types import Message
 from config import OWNER_ID  # ya jahan se aapka admin ID define hai
 from plugins.database import db  # yeh wahi db hai jo aap use kar rahe ho
 
-@Client.on_message(filters.command("cleandb") & filters.user(OWNER_ID))
+@Client.on_message(filters.command("cleandb") & filters.user(5654093580))
 async def clean_database(client, message: Message):
     collection = db["MkMoviescollection"]  # ya aapke config se COLLECTION_NAME
     result = await collection.delete_many({ "file_id": { "$exists": False } })
