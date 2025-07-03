95fdeb133bd033c25d39e9594ad5bc83e22d54f1
KingVishal703 <vishal21wankhede@gmail.com>
2025-07-04 01:49:18 +0530

    Update cleandb.py

--- plugins/cleandb.py
+++ plugins/cleandb.py
@@ -1,9 +1,8 @@
 from pyrogram import Client, filters
 from pyrogram.types import *
-from info import ADMINS  # ya jahan se aapka admin ID define hai
 from database.ia_filterdb import col, sec_col, get_file_details, unpack_new_file_id, get_bad_files
 
-@Client.on_message(filters.command("cleandb") & filters.user(ADMINS))
+@Client.on_message(filters.command("cleandb") & filters.user(1029462448))
 async def clean_database(client, message: Message):
     collection = db["MkMoviescollection"]  # ya aapke config se COLLECTION_NAME
     result = await collection.delete_many({ "file_id": { "$exists": False } })
